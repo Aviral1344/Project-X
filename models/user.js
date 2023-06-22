1a4a9d48dae1e4e41bb860322926eaf37d92826e
Aviral1344 <aviralgupta1344@gmail.com>
2023-06-22 11:57:33 +0530

    env setup

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const mongoose= require('mongoose')
 const bcrypt= require('bcrypt')
 
+const dotenv = require('dotenv')
+dotenv.config()
+
 mongoose.set('strictQuery', true);
-uri="mongodb+srv://aviral_1344:qwerty6789@konekt.xxeqmsg.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(uri)
+const uri= process.env.SERVER;
+mongoose.connect(uri);
 var dbs = mongoose.connection
 dbs.on('error',()=>console.log("error in connecting to database"));
 dbs.once('open',()=>console.log("Connection to MongoDB database successful :)"))
